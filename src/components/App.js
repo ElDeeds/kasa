88d8bf83985d77cc65efc9b7ede5c719f89da239
ElDeeds <126665497+ElDeeds@users.noreply.github.com>
2023-08-11 11:07:09 +0200

    add /kasa route

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,9 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Due à GitHub */}
+        <Route path="/kasa" element={<Index />} />
+        {/* Routes */}
         <Route path="/" element={<Index />} />
         <Route path="/logements/:id" element={<LogementsID />} />
         <Route path="/about" element={<About />} />
