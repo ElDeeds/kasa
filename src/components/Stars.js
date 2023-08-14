94c017d7de77c84f09697c3d2818ddffbf51455a
ElDeeds <126665497+ElDeeds@users.noreply.github.com>
2023-08-14 13:09:56 +0200

    map

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,16 +1,16 @@
 function StarRating({ rating }) {
-    const starCount = 5;
-  
-    const stars = Array.from({ length: starCount }, (_, index) => (
-      <span
-        key={index}
-        className={`star ${index < rating ? 'filled' : ''}`}
-      >
-        &#9733;
-      </span>
-    ));
-  
-    return <div className="starRating">{stars}</div>;
-  }
+  const starCount = 5;
 
-  export default StarRating;
+  const stars = Array(starCount).fill(null).map((_, index) => (
+    <span
+      key={index}
+      className={`star ${index < rating ? 'filled' : ''}`}
+    >
+      &#9733;
+    </span>
+  ));
+
+  return <div className="starRating">{stars}</div>;
+}
+
+export default StarRating;
